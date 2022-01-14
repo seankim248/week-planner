c96fb4c81e6b247f5c6f306453293554ef71aafe
Sean Kim <seankim2480@gmail.com>
2022-01-14 23:48:50 +0000

    users are able to view each entry from each day

--- main.js
+++ main.js
@@ -9,7 +9,33 @@ function showEntryFunction(event) {
 var $form = document.querySelector('form');
 $form.addEventListener('submit', submitFunction);
 
-var entries = [];
+var entries = [
+  {
+    day: 'Monday',
+    time: '02:00',
+    description: 'It is Monday.'
+  },
+  {
+    day: 'Tuesday',
+    time: '03:00',
+    description: 'Yo wassup'
+  },
+  {
+    day: 'Wednesday',
+    time: '04:00',
+    description: 'How ya doin'
+  },
+  {
+    day: 'Thursday',
+    time: '07:00',
+    description: 'This is feature 2'
+  },
+  {
+    day: 'Friday',
+    time: '12:00',
+    description: 'ok'
+  }
+];
 function submitFunction(event) {
   event.preventDefault();
   var $overlay = document.querySelector('.overlay');
@@ -26,6 +52,33 @@ function submitFunction(event) {
     description: $descriptionValue
   };
   entries.push(entryData);
+
   $form.reset();
-  console.log(entries);
 }
+
+function getEntries(dayOfWeek) {
+  var dayData = [];
+  for (var i = 0; i < entries.length; i++) {
+    if (entries[i].day === dayOfWeek) {
+      dayData.push(entries[i].time);
+      dayData.push(entries[i].description);
+    }
+  }
+  return dayData;
+}
+
+function renderEntries(arr) {
+  var $tdList = document.querySelectorAll('tbody td');
+  for (var i = 0; i < $tdList.length; i++) {
+    $tdList[i].textContent = arr[i];
+  }
+}
+
+renderEntries(getEntries('Monday'));
+
+var $weekdaysBox = document.querySelector('.weekdays-box');
+$weekdaysBox.addEventListener('click', function (e) {
+  renderEntries(getEntries(e.target.textContent));
+  var $span = document.querySelector('span');
+  $span.textContent = e.target.textContent;
+});
