c9ab3490d61ce8d8d3cca62e58779793fa97bcb4
edtan094 <etan094@gmail.com>
2022-01-14 12:48:00 -0800

    Hello there.

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-// var addEntryButton = document.querySelector('.add-entry-button');
+var $addEntryButton = document.querySelector('.add-entry-button');
+$addEntryButton.addEventListener('click', showEntryFunction);
+
+function showEntryFunction(event) {
+  var $overlay = document.querySelector('.overlay');
+  $overlay.classList.replace('hidden', 'display');
+}
+
+var $form = document.querySelector('form');
+$form.addEventListener('submit', submitFunction);
+
+var entries = [];
+function submitFunction(event) {
+  event.preventDefault();
+  var $overlay = document.querySelector('.overlay');
+  $overlay.classList.replace('display', 'hidden');
+  var $dayInput = document.querySelector('.day-of-week');
+  var $dayInputValue = $dayInput.value;
+  var $time = document.querySelector('.time');
+  var $timeValue = $time.value;
+  var $description = document.querySelector('textarea');
+  var $descriptionValue = $description.value;
+  var entryData = {
+    day: $dayInputValue,
+    time: $timeValue,
+    description: $descriptionValue
+  };
+  entries.push(entryData);
+  $form.reset();
+  console.log(entries);
+}
